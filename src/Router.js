9665e88207bf31923fe8a72b594b161e56b227d4
Sam Thomson <samt@samt.st>
2017-12-14 23:05:21 +0700

    login/employee page, with routing

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import { Scene, Router, Stack } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Stack key="root">
-                <Scene
-                    key="login"
-                    component={LoginForm}
-                    title="Please login"
-                />
-            </Stack>
+            <Scene key="root" modal hideNavBar>
+                <Scene key="auth" initial title="Please Login" >
+                    <Scene key="login" component={LoginForm} />
+                </Scene>
+                <Scene key="main">
+                    <Scene
+                        key="employeeList"
+                        component={EmployeeList}
+                        title="Employees"
+                        rightTitle="Add"
+                        // onRight={() => Actions.employeeCreate()}
+                        initial
+                    />
+                    {/* <Scene
+                        key="employeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"
+                    /> */}
+                </Scene>
+            </Scene>
         </Router>
     )
 }
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+                <Text>employee list</Text>
+            </View>
+        )
+    }
+}
+
+export default EmployeeList
